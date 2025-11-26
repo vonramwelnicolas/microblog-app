b42551eff0899804d2e68a92c6145b01042fa8f2
Von Nicolas <vonramwelnicolas@gmail.com>
2025-11-26 11:46:04 +0800

    chore: deployment exercise

--- microblog/backend/config/routes.rb
+++ microblog/backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: proc { |env| [200, {}, ['<html><body><h1>Rails is running!</h1></body></html>']] }
+
+  get '/arbitrary', to: proc { |env| [200, {'Content-Type' => 'application/json'}, ['{"message": "This is arbitrary JSON", "timestamp": "' + Time.current.to_s + '", "random": ' + rand(1000).to_s + '}']] }
 
   namespace :api do
     resources :users, only: [:create, :index]
